afcc0462bd92ddded60c5fbba64ca8c0f098df45
Cameron Tacklind <cameron@tacklind.com>
2016-09-13 13:46:35 -0700

    Hello World express server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
 
-console.log('Hello World!');
+let app = express();
+let server = http.Server(app);
 
-setInterval(() => {
-  console.log(new Date());
-}, 1000 * 60);
+server.listen(
+  process.env.npm_package_config_socket || process.env.npm_package_config_port || process.env.npm_package_config_listen,
+  process.env.npm_package_config_hostname,
+  () => {
+    console.log('Listening at:', server.address());
+  }
+);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
