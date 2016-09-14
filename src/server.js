c6e8fb02496cfda195521a816c8d9346233f8ea8
Cameron Tacklind <cameron@tacklind.com>
2016-09-14 15:33:28 -0700

    Move optional hostname to own variable.

--- src/server.js
+++ src/server.js
@@ -8,11 +8,12 @@ let app = express();
 let server = http.Server(app);
 
 let listen = process.env.npm_package_config_socket || process.env.npm_package_config_port || process.env.npm_package_config_listen || 9000;
+let hostname = process.env.npm_package_config_hostname;
 
 let StartListening = server.listen.bind(
   server,
   listen,
-  process.env.npm_package_config_hostname,
+  hostname,
   () => {
     console.log('Listening at:', server.address());
   }
