fa2d54826e82218eec0af31adfac36ee9366b923
Cameron Tacklind <cameron@tacklind.com>
2016-09-14 15:37:04 -0700

    Add optional trailing commas

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ module.exports = {
     entry: './src/client.js',
     output: {
         path: __dirname + '/webpack',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: 'style!css' }
-        ]
-    }
+            { test: /\.css$/, loader: 'style!css' },
+        ],
+    },
 };
