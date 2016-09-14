727e0fc503046ce0b54ac8665d8be3a0f60e0017
Cameron Tacklind <cameron@tacklind.com>
2016-09-14 15:25:13 -0700

    Cleanup broken sockets

--- src/server.js
+++ src/server.js
@@ -17,11 +17,29 @@ let StartListening = server.listen.bind(
 );
 
 server.on('error', err => {
-  if (err.port) {
-    console.log(`Error: Address (${err.address} ${err.port}) already in use.`);
+  if (err.code == 'EADDRINUSE') {
+    if (err.port > 0) {
+      console.log(`Error: Address (${err.address} ${err.port}) already in use.`);
+    } else if (err.address) {
+      // Try to connect. If not running, remove and continue.
+
+      var clientSocket = new net.Socket();
+
+      clientSocket.on('error', e => {
+        if (e.code == 'ECONNREFUSED') {
+          fs.unlinkSync(err.address);
+          StartListening();
+        }
+      });
+      clientSocket.connect({path: err.address}, () => {
+        console.log('Server running. Giving up...');
+        clientSocket.unref();
+      });
+    } else {
+      throw err;
+    }
   } else {
-    console.log('Other Error:');
-    console.log(err);
+    throw err;
   }
 });
 
