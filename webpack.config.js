89bfd9836f18a8feb584382429df165537879f94
Cameron Tacklind <cameron@tacklind.com>
2016-09-14 15:59:33 -0700

    Add babel-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.css$/, loader: 'style!css' },
+            { test: /\.jsx?$/, loader: 'babel'},
         ],
     },
     plugins: [
