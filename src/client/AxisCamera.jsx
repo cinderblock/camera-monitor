945bc0af092dcdefa27acee4b360073bff562598
Cameron Tacklind <cameron@tacklind.com>
2016-09-15 17:13:28 -0700

    Include react-bootstrap

--- src/client/AxisCamera.jsx
+++ src/client/AxisCamera.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Button} from 'react-bootstrap';
 
 export default class AxisCamera extends React.Component {
   render() {
     return (
       <div className="axisCamera">
+        <Button>Up</Button>
       </div>
     );
   }
