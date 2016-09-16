e9ab608558a8fae116344cfeeda3ba0fd53f3cbe
Cameron Tacklind <cameron@tacklind.com>
2016-09-15 17:10:32 -0700

    Cleanup

--- src/client.jsx
+++ src/client.jsx
@@ -2,8 +2,4 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import MonitorContainer from './client/MonitorContainer.jsx';
 
-
-ReactDOM.render(
-  <MonitorContainer />,
-  document.getElementById('container')
-);
+ReactDOM.render(<MonitorContainer />, document.getElementById('container'));
