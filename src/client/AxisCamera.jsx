99e6e90fa787ee3bec87d4b905c9da74af1794b8
Cameron Tacklind <cameron@tacklind.com>
2016-09-20 16:42:20 -0700

    Add constructor to AxisCamera component

--- src/client/AxisCamera.jsx
+++ src/client/AxisCamera.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {Button} from 'react-bootstrap';
 
 export default class AxisCamera extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="axisCamera">
