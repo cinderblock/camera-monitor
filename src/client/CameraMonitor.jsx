7a7a33f13e47f9d07de1826836593f82d3958f7a
Cameron Tacklind <cameron@tacklind.com>
2016-09-24 01:28:24 -0700

    load list of cameras from server endpoint

--- src/client/CameraMonitor.jsx
+++ src/client/CameraMonitor.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import Camera from './Camera.jsx';
 
+import request from 'browser-request';
+
 export default class CameraMonitor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {cameraComponents: props.cameraComponents};
+  }
+  componentDidMount() {
+    var cameras = this.props.cameraComponents;
+
+    if (!cameras) {
+      request(this.props.serverOrigin + '/cameras', (err, res, body) => {
+        body = JSON.parse(body);
+        cameras = new Array(body.length);
+        for (let i = 0; i < body.length; i++) {
+          let opts = body[i];
+          cameras[i] = {
+            driver: opts.driver,
+            origin: this.props.serverOrigin + '/camera/' + i,
+          };
+        }
+        this.setState({cameraComponents: cameras});
+      });
+    }
   }
   render() {
 
