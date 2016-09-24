12837ec52b141d66630278f0b31a0bf82311bca2
Cameron Tacklind <cameron@tacklind.com>
2016-09-24 00:19:04 -0700

    Get rid of old reference to listen config option

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 export default {
   server: {
-    listen: process.env.npm_package_config_socket || process.env.npm_package_config_port || process.env.npm_package_config_listen || 9000,
+    listen: process.env.npm_package_config_socket || process.env.npm_package_config_port || 9000,
     hostname: process.env.npm_package_config_hostname,
     socketMode: process.env.npm_package_config_socketmode || process.env.socketmode,
     camerasFile: process.env.npm_package_config_camerasfile || '../cameras.json',
