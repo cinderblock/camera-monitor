52f77b2a38db3a90d300ba0810b361b632282f58
Cameron Tacklind <cameron@tacklind.com>
2016-09-23 23:30:42 -0700

    cleaner creation of new object

--- src/cameras.js
+++ src/cameras.js
@@ -5,9 +5,11 @@ var Cameras = [];
 import config from './config.js';
 
 require(config.server.camerasFile).forEach(options => {
+  let Camera = require(`./cameras/${options.driver}.js`);
+  let camera = new Camera(options);
   Cameras.push({
     options,
-    camera: new require(`./cameras/${options.driver}.js`)(options),
+    camera,
   });
 });
 
