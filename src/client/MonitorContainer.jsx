aa81231fc612da74ae833db80ca6e75619f387a1
Cameron Tacklind <cameron@tacklind.com>
2016-09-23 22:15:36 -0700

    cleaner

--- src/client/MonitorContainer.jsx
+++ src/client/MonitorContainer.jsx
@@ -4,8 +4,8 @@ import CameraMonitor from './CameraMonitor.jsx';
 let serverOrigin =
   process.env.npm_package_config_serverorigin ||
   (
-    'http://'+
-    (process.env.npm_package_config_hostname || 'localhost')+
+    'http://' +
+    (process.env.npm_package_config_hostname || 'localhost') +
     (process.env.npm_package_config_port || 9000)
   )
   ;
